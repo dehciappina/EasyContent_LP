449e5eb9220231f59063466d454a014c7710b980
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2021-05-25 17:03:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 
-  document.body.onscroll = function() {
-    let windowPosition = window.pageYOffset;
-      document.querySelector('.big_ilus').style.transform = "translateY(" + windowPosition / 10 + "px)";
-    }
+
+
+    
+document.onreadystatechange = function () {
+  if (document.readyState == "interactive" || document.readyState == "complete") {
+
+    document.querySelector('#loading_bg').style.opacity = 0;
+    document.querySelector('#loading_bg').style.visibility = 'hidden';
+
+    setTimeout(() => {
+      document.querySelector('#loading_bg').style.display = 'none';
+    }, 800);
+
+    document.body.onscroll = function() {
+      let windowPosition = window.pageYOffset;
+        document.querySelector('.big_ilus').style.transform = "translateY(" + windowPosition / 10 + "px)";
+      }
+  }
+}
